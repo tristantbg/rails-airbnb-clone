f11a7b0263bf1d7da62f9a469c78092df8d3f490
Clement Picard-Zivy <clementpicardzivy@gmail.com>
2017-08-15 18:09:47 +0200

    commit avant changement de DB schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-
+  get "users/:id/contracts/new", to: "contracts#new"
   get 'users', to: 'users#show'
   resources :contracts, only: [:index, :new, :create, :update]
-	resources :skills, only: [:index, :show]  
+	resources :skills, only: [:index, :show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'users#index'
 end
