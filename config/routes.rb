792a12e5fc84ff396f7fcd83a5db6b149169c326
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 09:28:43 +0200

    config

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :contracts, only: [:index, :new, :create, :update]
   
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 end
