d77e40dde9ce54475f6fb03267605d16aeb246f9
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 18:05:06 +0200

    fixed database ???

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contracts, only: [:index, :new, :create, :update]
+  resources :contracts, only: [:index, :create, :update]
 	resources :skills, only: [:index, :show]  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
