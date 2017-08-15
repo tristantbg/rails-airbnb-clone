6b4797531fca683692ab697a73e536f71f5cdc1a
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 16:00:49 +0200

    fixes nav user + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :contracts, only: [:show, :index, :new, :create, :update]
+  resources :users, only: [:show]
+  resources :contracts, only: [:index, :new, :create, :update]
 	resources :skills, only: [:index, :show]  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'users#index'
