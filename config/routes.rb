e0ebfc816f9c9f96dfd915812ab40a3ee1808f29
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 19:01:35 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
   resources :contracts, only: [:index, :create, :update]
 	resources :skills, only: [:index, :show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [:index, :show]
   root to: 'users#index'
 end
