115f7c0f34f98e8055bc092f8ea64cbe44f53962
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 10:53:23 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index]
+  get 'users', to: 'users#show'
   resources :contracts, only: [:index, :new, :create, :update]
   
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+  root to: 'users#index'
 end
