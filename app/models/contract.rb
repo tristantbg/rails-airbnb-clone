c9f2acc2d3286909b1cf01dfe05042b85d3d49a5
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-15 18:54:59 +0200

    home with cards

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,7 +2,7 @@ class Contract < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true, uniqueness: true
-  validates :status, presence: true, :inclusion => { :in => ['pending', 'accepted', 'rejected'] },
+  validates :status, presence: true, :inclusion => { :in => ['pending', 'accepted', 'rejected'] }
   validates :start_time, presence: true
   validates :end_time, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :contracts, only: [:index, :create, :update]
 	resources :skills, only: [:index, :show]  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:show]
   root to: 'users#index'
 end
