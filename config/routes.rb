95e73f63275e27109780582f8a00a43fd5038f15
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-16 12:08:15 +0200

    controller + routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	resources :skills, only: [:index, :show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:index, :show] do
-    resources :contracts, only: [:index, :create, :update]
+    resources :contracts, only: [:index, :new, :create, :update]
   end
   root to: 'users#index'
 end
