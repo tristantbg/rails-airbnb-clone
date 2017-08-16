8174605776d9afcdbe598fd4e69649124f51ffd7
Sophia Wushanley <swushanley@gmail.com>
2017-08-16 12:40:19 +0200

    changed structure of profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
       if params[:skill].present?
           @users = @users.reject{ |user| Skill.find_by(user_id: user.id).name.downcase != params[:skill].downcase }
       end
+  def show
+   @user = User.find(params[:id])
+  end
   end
 end
