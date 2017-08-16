73bd30dc7d8f6bcc0480d70d7ba7680268046d5a
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-16 12:38:49 +0200

    routes

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -14,8 +14,6 @@ class ContractsController < ApplicationController
 
   def create
     @contract = Contract.new(contract_params)
-    @contract.employer_id = current_user.id
-    
     if @contract.save
       redirect_to contracts_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-	resources :skills, only: [:index, :show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:index, :show] do
     resources :contracts, only: [:index, :new, :create, :update]
