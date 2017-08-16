32a491a6136d95192c5d904fddea17a87263ffc8
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-16 16:04:22 +0200

    routes + show ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-   @user = User.find(params[:id])
-   @skill = @user.skills.first
+    @user = User.find(params[:id])
+    @skill = @user.skills.first
   end
 
   def index
-      @users = User.all.in_location(params[:location])
-      if params[:skill].present?
-          @users = @users.reject{ |user| Skill.find_by(user_id: user.id).name.downcase != params[:skill].downcase }
-      end
-  def show
-   @user = User.find(params[:id])
-  end
+    @users = User.all.in_location(params[:location])
+    if params[:skill].present?
+      @users = @users.reject{ |user| Skill.find_by(user_id: user.id).name.downcase != params[:skill].downcase }
+    end
   end
 
 end
