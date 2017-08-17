7f815761b7fbed73cee34e0f1847f8bf152ac944
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-17 15:09:12 +0200

    button switch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,25 +3,19 @@
 //= require bootstrap-sprockets
 //= require bootstrap-switch
 //= require_tree .
-
-
-$(document).ready(function(){	
-$("[name='freelance-response']").bootstrapSwitch({
-    size: 'small',
-    onText: '',
-    offText: '',
-    labelText: '<i class="fa fa-circle"></i>'
+$(document).ready(function() {
+    $("[name='freelance-response']").bootstrapSwitch({
+        size: 'small',
+        onText: '<i class="fa fa-circle"></i>',
+        offText: '<i class="fa fa-circle"></i>',
+        labelText: '<i class="target fa fa-circle"></i>'
+    });
+    $('input[name="freelance-response"]').on('switchChange.bootstrapSwitch', function(event, state) {
+        $switch = $(this).parents(".bootstrap-switch-container").find('i.target');
+        if (state) {
+            $switch.attr('class', 'target fa fa-check-circle');
+        } else {
+            $switch.attr('class', 'target fa fa-times-circle');
+        }
+    });
 });
-
-$('input[name="freelance-response"]').on('switchChange.bootstrapSwitch', function(event, state) {
-    $switch = $(this).parents(".bootstrap-switch-container").find('i.fa');
-    console.log($switch);
-        console.log($(this));
-
-    if (state) {
-    	$switch.attr('class', 'fa fa-check-circle');
-    } else {
-    	$switch.attr('class', 'fa fa-times-circle');
-    }
-});
-})
